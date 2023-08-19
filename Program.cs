2c04935981052289c8e5767b06ee9119dd832571
john2708 <jbmiranda22796@gmail.com>
2023-08-19 13:06:42 -0700

    add git ignore

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>
+// wire in DataContext.cs file
+builder.Services.AddDbContext<DataContext> 
 
 var app = builder.Build();
 
