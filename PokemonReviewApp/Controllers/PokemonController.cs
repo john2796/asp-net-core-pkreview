ffb6b4d95e23a338fe5c1503db644dcf4f6ccdae
john2708 <jbmiranda22796@gmail.com>
2023-08-20 10:36:21 -0700

    review repository and interface

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -25,22 +25,25 @@ using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
 
 namespace PokemonReviewApp.Controllers
-{   
+{
     // these are attributes to turn them to controller
     [Route("api/[controller]")]
     [ApiController]
-    public class PokemonController: Controller
+    public class PokemonController : Controller
     {
         // access to Repository methods
         private readonly IPokemonRepository _pokemonRepository;
         private readonly IMapper _mapper;
+        private readonly IReviewRepository _reviewRepository;
 
 
         // bring repository
-        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
+        public PokemonController(IPokemonRepository pokemonRepository, IReviewRepository reviewRepository, IMapper mapper)
         {
             _pokemonRepository = pokemonRepository;
             _mapper = mapper;
+            _reviewRepository = reviewRepository;
+
         }
 
         // endpoints CRUD
@@ -60,11 +63,13 @@ namespace PokemonReviewApp.Controllers
         [HttpGet("{pokeId}")]
         [ProducesResponseType(200, Type = typeof(Pokemon))]
         [ProducesResponseType(400)]
+        // IActionResult: lets us NotFound, BadRequest, basically action output result
         public IActionResult GetPokemon(int pokeId)
         {
             if (!_pokemonRepository.PokemonExists(pokeId)) return NotFound();
+            // _mapper: make properties of PokemonDto to returned result
             var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
-
+            // do validation
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
             return Ok(pokemon);
@@ -79,8 +84,73 @@ namespace PokemonReviewApp.Controllers
             var rating = _pokemonRepository.GetPokemonRating(pokeId);
             // validation on Dto , client value etc.
             if (!ModelState.IsValid) return BadRequest(ModelState);
-    
+
             return Ok(rating);
         }
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreatePokemen([FromQuery] int ownerId, [FromQuery] int catId, [FromBody] PokemonDto pokemonCreate)
+        {
+            if (pokemonCreate == null) return BadRequest(ModelState);
+
+            var pokemons = _pokemonRepository.GetPokemonTrimToUpper(pokemonCreate);
+
+            if (pokemons != null)
+            {
+                ModelState.AddModelError("", "Owner already exist");
+                return StatusCode(422, ModelState);
+            }
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var pokemonMap = _mapper.Map<PokemonDto>(pokemonCreate);
+
+            if (!_pokemonRepository.CreatePokemon(ownerId, catId, pokemonMap))
+            {
+                ModelState.AddModelError("", "Something when wrong while saving");
+                return StatusCode(500, ModelState);
+            }
+            return Ok("Successfully created");
+        }
+
+        [HttpPut("{pokeId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdatePokemon(int pokeId, [FromQuery] int ownerId, [FromBody] PokemonDto updatePokemon)
+        {
+            if (updatePokemon == null ) return BadRequest(ModelState);   
+            if (pokeId !== updatePokemon.Id) return BadRequest(ModelState);
+            if (!_pokemonRepository.PokemonExists(pokeId)) return NotFound();
+
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var pokemonMap = _mapper.Map<Pokemon>(updatedPokemon);
+            if(!_pokemonRepository.UpdatePokemon(ownerId, catId, pokemonMap))
+            {
+                ModelState.AddModelError("", "something went wrong updating owner");
+                return StatusCode(500, ModelState)
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{pokeId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+
+        public IActionResult DeletePokmeon(int pokeId)
+        {
+            if(!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+           // var reviewsToDelete = _pokemonRepository.GetRev
+
+           if(!_pokemonRepository.DeletePokemon(poke)
+        }
+
     }
 }
--- PokemonReviewApp/Interfaces/IReviewRepository.cs
+++ PokemonReviewApp/Interfaces/IReviewRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace PokemonReviewApp.Interfaces
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
 {
-    public interface IReviewRepository
+   public interface IReviewRepository
     {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfAPokemon(int pokeId);
+        bool ReviewExisst(int reviewId);
+        bool CreateReview(Review review);
+        bool UpdateReview(Review review);
+        bool DeleteReview(Review review);
+        bool DeleteReviews(List<Review> reviews);
+        bool Save();
     }
 }
